ecc56c0a96f76efc8affea2d217469c19ed818b3
Durchschnittsgeschwindigkeit <cl_s3226541050@soshi.jp>
2024-05-22 14:09:46 +0900

    testooooooooo

--- PUTSCH!/ViewController.swift
+++ PUTSCH!/ViewController.swift
@@ -35,23 +35,30 @@ class ViewController: UIViewController {
         
     }
     @IBAction func shutudai(){
+        //ボタン押した時ランダムに式
         switch Int.random(in:1..<3){
-        case 1:hyouji()
+        case 1:
+            hyouji()
             mondai.text=String(nijoubig)+"*"+String(nijousmall)
             kotae.text=String(nijoubig*nijousmall)
-        case 2:keisuu=[2]
+        case 2:
+            let bonchi=Int.random(in:1..<10)*100
+            let manji=Int.random(in:1..<10)*100
+            mondai.text=String(bonchi)+"^"+String(manji)
+            kotae.text=String(ruijou(kisuu:bonchi,shisuu:manji))
         default:break
         }
     }
     func ruijou(kisuu: Int,shisuu: Int)->Int{
         if shisuu==0 {
             return 1
+            //基数、指数の順で入れると累乗ができる
         }
         return kisuu*ruijou(kisuu:kisuu,shisuu:shisuu-1)
     }
     func keta(ketasuu: Int)->Int{
-        //var jijiji=ruijou(kisuu:10,shisuu:ketasuu)
-       return  Int.random(in: ruijou(kisuu:10,shisuu:ketasuu)..<10)
+        //桁数を入れるとその桁の乱数が出てくる
+       return  Int.random(in: ruijou(kisuu:10,shisuu:ketasuu)..<ruijou(kisuu:10,shisuu:ketasuu)*10)
     }
     func hyouji (){
         let randombig=Int.random(in:1..<10)*100
