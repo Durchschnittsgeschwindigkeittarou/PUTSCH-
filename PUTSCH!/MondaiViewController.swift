2e7c9a39d2c3a1516ef7cf718e411318470bd9ab
ren8978 <takuma.yoshikawa@icloud.com>
2024-06-13 14:41:17 +0900

    kakeru

---
+++ PUTSCH!/MondaiViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MondaiViewController.swift
+//  PUTSCH!
+//
+//  Created by clark on 2024/06/07.
+//
+
+import UIKit
+
+class MondaiViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
