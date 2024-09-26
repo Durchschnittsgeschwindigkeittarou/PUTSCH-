4116acecb12bc8e023c97fd6d19cdd14d73ab381
ren8978 <takuma.yoshikawa@icloud.com>
2024-09-26 15:11:15 +0900

    result

---
+++ PUTSCH!/resultViewController.swift
@@ -0,0 +1,29 @@
+//
+//  resultViewController.swift
+//  PUTSCH!
+//
+//  Created by clark on 2024/09/26.
+//
+
+import UIKit
+
+class resultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
